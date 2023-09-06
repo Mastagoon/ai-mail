7e9b8d77d4707ccbd5f14726da24725cfeb18c54
Mastagoon <inzilagiway@gmail.com>
2023-09-06 11:42:10 +0300

    speech page, dotenv setup

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,5 +3,8 @@
 </script>
 
 <div class="container mx-auto my-2 h-full w-full">
+	<div class="flex flex-row mb-12">
+		<img src="logo.svg" alt="">
+	</div>
 	<slot />
 </div>
---
+++ src/routes/speech/+page.svelte
@@ -0,0 +1,19 @@
+<div class="flex flex-col gap-4">
+	<div class="flex flex-row justify-center gap-8 items-center">
+		<h1 class="text-2xl font-bold">Recording...</h1>
+		<button class="border-[1px] px-4 py-3 border-[#19231A] rounded-[5px] flex 
+		hover:text-white
+		flex-row items-center gap-4 hover:bg-secondary">
+			Pause
+		</button>
+	</div>
+	<textarea class="bg-[#D9D9D9] rounded-sm resize-none" disabled rows={25}></textarea>
+
+	<div class="flex flex-row justify-center">
+		<button class="border-[1px] px-4 py-3 border-[#19231A] rounded-[5px] flex 
+		hover:text-white
+		flex-row items-center gap-4 hover:bg-secondary">
+			Send
+		</button>
+	</div>
+</div>

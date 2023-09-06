112bbe750900c6e9466753d78cc31792752dba91
Mastagoon <inzilagiway@gmail.com>
2023-09-06 11:29:30 +0300

    homepage design

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,6 @@
   import "../app.css";
 </script>
 
-<slot />
+<div class="container mx-auto my-2 h-full w-full">
+	<slot />
+</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: "#CBBEB3",
+				secondary: "#0A1A50",
+			}
+		},
 	},
 	plugins: [],
 }

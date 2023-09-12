477563e04dd033e3da7f0e2071a471d29ade9e03
Mastagoon <inzilagiway@gmail.com>
2023-09-12 16:31:49 +0300

    wip

--- src/routes/type/+page.svelte
+++ src/routes/type/+page.svelte
@@ -3,12 +3,17 @@
   import { onMount } from 'svelte';
 	import { enhance } from '$app/forms';
 	import Loading from "../../components/Loading.svelte";
+    import { reset } from "__sveltekit/paths";
 
 	export let form
 	let isLoading = false;
 
   let ready = false;
   onMount(() => ready = true);
+
+	const handleSend = () => {
+
+	}
 </script>
 
 {#if ready}
@@ -29,16 +34,28 @@
 					value="This is a business email, you are the CEO of a company"
 					name="context" class="bg-[#D9D9D9] rounded-sm resize-none p-4" rows={2}></textarea>
 			</div>
+
+{#if form?.result}
 		<textarea name="prompt" class="bg-[#D9D9D9] rounded-sm resize-none p-4" rows={6}></textarea>
 			<span>Response:</span>
 		<textarea value={form?.result??""} name="response" class="bg-[#D9D9D9] rounded-sm resize-none p-4" rows={12}></textarea>
-
-		<div class="flex flex-row justify-center">
-			<button type="submit" class="border-[1px] px-4 py-3 border-[#19231A] rounded-[5px] flex 
+{/if}
+		<div class="flex flex-row justify-center gap-8 bg-red-500">
+			<button
+					type="submit" class="border-[1px] px-4 py-3 border-[#19231A] rounded-[5px] flex 
 			hover:text-white
 			flex-row items-center gap-4 hover:bg-secondary">
-				Send
+				Generate
 			</button>
+{#if form?.result}
+		<button
+			on:click={handleSend}
+			type="submit" class="border-[1px] px-4 py-3 border-[#19231A] rounded-[5px] flex 
+			hover:text-white
+			flex-row items-center gap-4 hover:bg-secondary">
+				Send mail
+		</button>
+{/if}
 		</div>
 	</form>
 
@@ -47,6 +64,7 @@
 {#if isLoading}
 		<Loading/>
 {/if}
+
 {#if form?.error}
 	<p in:slide={{duration:500}} class="text-red-500 text-center">{form?.error}</p>
 {/if}

c75d404b14d9e8d501d4ce94d0af87ac6bf44056
Mastagoon <inzilagiway@gmail.com>
2023-09-13 19:05:40 +0300

    submit with ctrl+enter

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,7 +4,9 @@
 
 <div class="container mx-auto my-2 h-full w-full">
 	<div class="flex flex-row mb-12">
-		<img src="logo.svg" alt="">
+		<a href="/">
+			<img src="logo.svg" alt="">
+		</a>
 	</div>
 	<slot />
 </div>
--- src/routes/type/+page.svelte
+++ src/routes/type/+page.svelte
@@ -1,9 +1,8 @@
-<script>
+<script lang="ts">
   import { slide} from "svelte/transition";
   import { onMount } from 'svelte';
 	import { enhance } from '$app/forms';
 	import Loading from "../../components/Loading.svelte";
-    import { reset } from "__sveltekit/paths";
 
 	export let form
 	let isLoading = false;
@@ -11,41 +10,52 @@
   let ready = false;
   onMount(() => ready = true);
 
+	let  formRef: HTMLFormElement;
+
 	const handleSend = () => {
 
 	}
 </script>
 
 {#if ready}
-<div class="flex flex-col gap-4">
+<div 
+		on:keypress={e => {
+			if (e.key === "Enter" && e.ctrlKey) {
+				formRef.requestSubmit()
+			}
+		}}
+		class="flex flex-col gap-4">
 	<div class="flex flex-row justify-center gap-8 items-center">
 		<h1 class="text-2xl font-bold capitalize">Write an email prompt</h1>
 	</div>
-	<form method="POST" class="flex flex-col gap-4" use:enhance={() => {
-		isLoading = true
-		return async ({ update }) => {
-			await update({ reset: false });
-			isLoading = false
-		};
+	<form 
+			bind:this={formRef}
+			method="POST" class="flex flex-col gap-4" use:enhance={() => {
+				isLoading = true
+				return async ({ update }) => {
+				await update({ reset: false });
+				isLoading = false
+			};
 	}}>
 			<div class="flex flex-col gap-1">
-				<label class="text-lg " for="context">{isLoading}</label>
+				<label class="text-lg " for="context">Context</label>
 				<textarea 
 					value="This is a business email, you are the CEO of a company"
 					name="context" class="bg-[#D9D9D9] rounded-sm resize-none p-4" rows={2}></textarea>
 			</div>
 
-{#if form?.result}
+		<span>Prompt</span>
 		<textarea name="prompt" class="bg-[#D9D9D9] rounded-sm resize-none p-4" rows={6}></textarea>
+{#if form?.result}
 			<span>Response:</span>
 		<textarea value={form?.result??""} name="response" class="bg-[#D9D9D9] rounded-sm resize-none p-4" rows={12}></textarea>
 {/if}
-		<div class="flex flex-row justify-center gap-8 bg-red-500">
+		<div class="flex flex-row justify-center gap-8 ">
 			<button
 					type="submit" class="border-[1px] px-4 py-3 border-[#19231A] rounded-[5px] flex 
 			hover:text-white
 			flex-row items-center gap-4 hover:bg-secondary">
-				Generate
+					{form?.result ? "Regenerate" : "Generate"}
 			</button>
 {#if form?.result}
 		<button
@@ -62,7 +72,7 @@
 </div>
 
 {#if isLoading}
-		<Loading/>
+		<Loading t="Generating your email..."/>
 {/if}
 
 {#if form?.error}

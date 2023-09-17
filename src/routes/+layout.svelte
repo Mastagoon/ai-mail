4631c2733beadb821fc3451c598068b2bb22a8e3
Mastagoon <inzilagiway@gmail.com>
2023-09-17 20:53:16 +0300

    head ele

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,12 @@
   import "../app.css";
 </script>
 
+<svelte:head>
+	<title>AI Mail</title>
+	<meta name="robots" content="noindex nofollow" />
+	<html lang="en" />
+</svelte:head>
+
 <div class="container mx-auto my-2 h-full w-full">
 	<div class="flex flex-row mb-12">
 		<a href="/">
